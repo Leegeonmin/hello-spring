b68b14ef43a3f517961fbceb11134ef934c07770
LSGeonmin <lkm9709@portlogics.com>
2023-08-27 03:43:53 +0900

    feat : MemberService에서 Repository DI

--- src/main/java/com/hello/hellospring/service/MemberService.java
+++ src/main/java/com/hello/hellospring/service/MemberService.java
@@ -2,14 +2,17 @@ package com.hello.hellospring.service;
 
 import com.hello.hellospring.domain.Member;
 import com.hello.hellospring.repository.MemberRepository;
-import com.hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService
 {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원가입
