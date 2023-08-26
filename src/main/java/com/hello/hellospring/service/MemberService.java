ab4933074d0f3a1eb47bf1432cc162516b1dd399
LSGeonmin <lkm9709@portlogics.com>
2023-08-27 03:09:07 +0900

    feat : MemberService 추가

---
+++ src/main/java/com/hello/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package com.hello.hellospring.service;
+
+import com.hello.hellospring.domain.Member;
+import com.hello.hellospring.repository.MemberRepository;
+import com.hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService
+{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        // 같은 이름이 있는 중복 회원 X
+        // 로직이 쭉 길어지면 메서드로 추출하자
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 젠처 회원 조회
+     * 서비스 클래스는 Business logic에 의존적으로 이름을 짓는다
+     */
+    public List<Member> findMembers()
+    {
+            return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 조회(ID로)
+     */
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+    private void validateDuplicateMember(Member member) {
+//        Optional<Member> result= memberRepository.findByName(member.getName());
+
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+}
