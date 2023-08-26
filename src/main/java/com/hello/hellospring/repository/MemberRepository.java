ae54b7f9baeb1df4505360f57a2d577c4be117cc
LSGeonmin <lkm9709@portlogics.com>
2023-08-27 01:52:50 +0900

    feat : Member 도메인 추가, MemberRepository 인터페이스 추가 및 MemoryMemberRepository 구현

---
+++ src/main/java/com/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.hello.hellospring.repository;
+
+import com.hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    // Optional, null 값을 감싸서 보내는 방법
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
