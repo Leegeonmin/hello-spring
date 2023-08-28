74ad5c6203a329e26fc669ae6295ae58474110f5
Leegeonmin <kmlee97s@gmail.com>
2023-08-28 16:42:30 +0900

    feat : H2 DB 연결 설정, MemberRepository -> JdbcMemberRepository 구현체로 DI 변경

--- src/main/java/com/hello/hellospring/SpringConfig.java
+++ src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,10 +1,15 @@
 package com.hello.hellospring;
 
+import com.hello.hellospring.repository.JdbcMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
 import com.hello.hellospring.repository.MemoryMemberRepository;
 import com.hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 /**
  * 컴포넌트 스캔이 아닌 방법으로 스프링에 클래스 등록하기
@@ -12,6 +17,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource =  dataSource;
+    }
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -19,6 +29,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
