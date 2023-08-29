207d197a84f51c6b7fbc64f464c9aff7e94a5fab
Leegeonmin <kmlee97s@gmail.com>
2023-08-29 16:19:45 +0900

    feat : 시간기록 AOP 추가

--- src/main/java/com/hello/hellospring/SpringConfig.java
+++ src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,10 +1,13 @@
 package com.hello.hellospring;
 
+import com.hello.hellospring.aop.TimeTraceAop;
 import com.hello.hellospring.repository.*;
 import com.hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.Time;
 
 /**
  * 컴포넌트 스캔이 아닌 방법으로 스프링에 클래스 등록하기
@@ -24,6 +27,10 @@ private  final MemberRepository memberRepository;
         return new MemberService(memberRepository);
     }
 
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 //    @Bean
 //    public MemberRepository memberRepository(){
 //
