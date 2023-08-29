c995304014ad444240e2479a8532166db55ca87f
Leegeonmin <kmlee97s@gmail.com>
2023-08-29 15:46:04 +0900

    feat : 스프링 데이터 JPA를 통해서 DB 접근하도록 변경

--- src/main/java/com/hello/hellospring/SpringConfig.java
+++ src/main/java/com/hello/hellospring/SpringConfig.java
@@ -6,38 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
-
 /**
  * 컴포넌트 스캔이 아닌 방법으로 스프링에 클래스 등록하기
  */
 @Configuration
 public class SpringConfig {
 
-//    private DataSource dataSource;
-//    @Autowired
-//    public SpringConfig(DataSource dataSource){
-//        this.dataSource =  dataSource;
-//    }
-    private EntityManager em ;
+private  final MemberRepository memberRepository;
+
     @Autowired
-    public SpringConfig(EntityManager em){
-        this.em= em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em );
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em );
+//        return new SpringDataJpaMemberRepository() ;
+//    }
 
 }
